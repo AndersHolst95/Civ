cae34e38edfc213059c0f7cf92c2f1968c572e28
Sheila <sheila@au.dk>
2019-10-01 11:47:43 +0200

    INCOMPLETE, Corrected clean code property.

--- src/main/java/hotciv/standard/GameImpl.java
+++ src/main/java/hotciv/standard/GameImpl.java
@@ -99,6 +99,11 @@ public class GameImpl implements Game {
         // Checking if anybody has won
         updateWinner();
 
+        // Iterate map
+        iterateMapEndOfRound();
+    }
+
+    private void iterateMapEndOfRound(){
         // Iterating over each tile on the map
         for(int i = 0; i < GameConstants.WORLDSIZE; i++){
             for(int j = 0; j< GameConstants.WORLDSIZE; j++){
@@ -116,6 +121,7 @@ public class GameImpl implements Game {
             }
         }
     }
+
 
     /**
      * Checks if a given city can produce a unit, and if so, tries to place it
